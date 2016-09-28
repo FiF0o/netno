591d3eb28948d7a1aa67f18a88b3017383fabc7a
jon <jonathan.lazarini@wipro.com>
2016-09-28 14:40:02 +0100

    fix reducer issue by adding json() on the promise

--- src/actions/getTweetsAsync.js
+++ src/actions/getTweetsAsync.js
@@ -1,5 +1,7 @@
 import { FETCHED_TWEET } from '../const'
 
+//https://dev.twitter.com/oauth/application-only
+
 const fetchedTweets = (tweets) => ({
   type: FETCHED_TWEET,
   tweets,
@@ -7,5 +9,26 @@ const fetchedTweets = (tweets) => ({
 
 export const getTweetsAsync = (query) => (
   (dispatch) => fetch(`http://localhost:2000/search?q=${query}`)
+      .then(data => data.json())
       .then(data => dispatch(fetchedTweets(data.statuses)))
 )
+
+
+/**
+ *
+ * const gettingTweets = (q = query) => {
+   // promisefy the codebird call otherwise getTweetsAsync is returned as
+   // undefined
+   return new Promise((resolve, reject) => {
+     const cb = new Codebird
+     cb.setConsumerKey(key, secret);
+     cb.__call("search_tweets", q, (reply, rate, err,) => {
+       console.log(reply)
+       console.log(rate)
+       resolve(reply);
+     }, true);
+  })
+ }
+ *
+ *
+*/
